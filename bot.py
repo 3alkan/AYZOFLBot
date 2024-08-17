a17910d52604f123d2904a392896db3f3313e633
Mustafa Alkan <3alkan@gmail.com>
2024-08-17 15:07:47 +0300

    initial

--- bot.py
+++ bot.py
@@ -1,20 +1,38 @@
+import os
+import traceback
+from dotenv import load_dotenv
 import discord
+from discord.ext import commands
+
+load_dotenv()
+token = os.getenv("AYZOFLBot_Token")
 
 intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+tree = bot.tree
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await tree.sync()
+    print('Commands synchronized globally.')
+    print(f"Logged in as {bot.user}")
+    for guild in bot.guilds:
+        print(f"Connected to guild: {guild.name} (ID: {guild.id})")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.event
+async def on_command_error(ctx, error):
+    print(f"An error occurred: {error}")
+    await ctx.send("An error occurred while processing the command.")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.event
+async def on_app_command_completion(interaction: discord.Interaction, command: discord.app_commands.Command):
+    print(f"Command '{command.name}' completed by user {interaction.user.name} in guild {interaction.guild.name}.")
 
-client.run('your token here')
+@tree.command(name="help", description="Displays help information.")
+async def help(interaction: discord.Interaction):
+    await interaction.response.send_message("this is help command")
+
+bot.run(token)
